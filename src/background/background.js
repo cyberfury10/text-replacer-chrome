ed7da6408d7c022f69100be5b22c823755c24beb
cyberfury10 <45261873+cyberfury10@users.noreply.github.com>
2023-08-19 04:22:36 +0000

    minor change

--- src/background/background.js
+++ src/background/background.js
@@ -1,9 +1,9 @@
-chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-  if (changeInfo.status === "complete") {
-    if (tab.url) {
-      chrome.tabs.sendMessage(tabId, {
-        state: `${tab.url} Loaded`,
-      })
-    }
-  }
-})
+// chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+//   if (changeInfo.status === "complete") {
+//     if (tab.url) {
+//       chrome.tabs.sendMessage(tabId, {
+//         state: `${tab.url} Loaded`,
+//       })
+//     }
+//   }
+// })
